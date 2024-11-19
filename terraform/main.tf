7250b7972473ba4013673782fe9c73ded7cfff02
Bartosz Lenartowicz <117129607+BartoszVGW@users.noreply.github.com>
2024-11-19 12:26:16 +1100

    Basic conf with bug

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,17 @@
 terraform {
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
       version = "~> 5.52.0"
     }
     random = {
       version = "~> 3.6.2"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.6.0"
+    }
+
   }
 
   cloud {
